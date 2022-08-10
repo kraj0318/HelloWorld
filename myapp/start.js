ea8757eb2a83bba3e95d6c6cf9ae409e4d9b04fe
Kumar Rituraj <kumar.rituraj@sap.com>
2022-08-10 15:38:24 +0000

    Initial commit
    Initial commit
    X

--- myapp/start.js
+++ myapp/start.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.send('Hello SAP World!!!');
 });
 
 const port = process.env.PORT || 4000;
